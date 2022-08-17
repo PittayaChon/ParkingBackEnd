523411f595a0a01436c6c3acc6be00434978c2b4
arigato <teeruch.k@gmail.com>
2022-08-17 15:59:26 +0700

    added create mock park endpoint

--- server.go
+++ server.go
@@ -7,20 +7,20 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
+var parks = []db.Park{
+	{LotId: "A001", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
+	{LotId: "A002", Licenseplate: "", Status: 1, Reserveable: false, Floor: "A"},
+	{LotId: "A003", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
+	{LotId: "A004", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
+	{LotId: "A005", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
+	{LotId: "A006", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
+	{LotId: "A007", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
+	{LotId: "A008", Licenseplate: "", Status: 0, Reserveable: true, Floor: "A"},
+	{LotId: "A009", Licenseplate: "", Status: 0, Reserveable: true, Floor: "A"},
+	{LotId: "A010", Licenseplate: "", Status: 0, Reserveable: true, Floor: "A"},
+}
 
-	var parks = []db.Park{
-		{LotId: "A001", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
-		{LotId: "A002", Licenseplate: "", Status: 1, Reserveable: false, Floor: "A"},
-		{LotId: "A003", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
-		{LotId: "A004", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
-		{LotId: "A005", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
-		{LotId: "A006", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
-		{LotId: "A007", Licenseplate: "", Status: 0, Reserveable: false, Floor: "A"},
-		{LotId: "A008", Licenseplate: "", Status: 0, Reserveable: true, Floor: "A"},
-		{LotId: "A009", Licenseplate: "", Status: 0, Reserveable: true, Floor: "A"},
-		{LotId: "A010", Licenseplate: "", Status: 0, Reserveable: true, Floor: "A"},
-	}
+func main() {
 
 	db.DB()
 	// test webhook 4
@@ -30,10 +30,21 @@ func main() {
 		return c.String(http.StatusOK, "Hi")
 	})
 
-	e.GET("parks", func(c echo.Context) error {
+	e.GET("/parks", func(c echo.Context) error {
+
+		var parks []db.Park
+
+		db.DB().Find(&parks)
+
+		return c.JSON(http.StatusOK, &parks)
+	})
+
+	e.POST("/create-mock-parks", func(c echo.Context) error {
 		parks := parks
 
-		return c.JSON(http.StatusOK, parks)
+		db.DB().Create(&parks)
+
+		return c.JSON(http.StatusCreated, parks)
 	})
 
 	e.Logger.Fatal(e.Start(":1234"))
